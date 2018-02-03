f2463268051bfa8a0c5dcaead18bb86c1daf5440
Allan Wang <me@allanwang.ca>
2018-02-03 18:38:12 -0500

    Add login options

--- reddit.py
+++ reddit.py
@@ -1,5 +1,4 @@
 import logging
-from pprint import pprint
 
 import praw
 
@@ -12,33 +11,53 @@ logger = logging.getLogger('retroreddit')
 logger.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
-rr = praw.Reddit(client_id=conf.clientId,
-                 client_secret=conf.clientSecret,
-                 user_agent=conf.userAgent)
+
+def createReddit(username=None, password=None):
+    if username is not None and password is not None:
+        logger.debug("Logging in as {}".format(username))
+        reddit = praw.Reddit(client_id=conf.clientId,
+                           client_secret=conf.clientSecret,
+                           user_agent=conf.userAgent,
+                           username=username,
+                           password=password)
+        if reddit.read_only:
+            logger.error("Failed to log in with user")
+            return None
+        return reddit
+    if hasattr(conf, 'testUser') and conf.testUser is not None and hasattr(conf,
+                                                                           'testPass') and conf.testPass is not None:
+        return createReddit(conf.testUser, conf.testPass)
+    return praw.Reddit(client_id=conf.clientId,
+                       client_secret=conf.clientSecret,
+                       user_agent=conf.userAgent)
+
+
+rr = createReddit()
 
 
 def login(username, password):
     global rr
-    logger.debug("Logging in as {}".format(username))
-    rr = praw.Reddit(client_id=conf.clientId,
-                     client_secret=conf.clientSecret,
-                     user_agent=conf.userAgent,
-                     username=username,
-                     password=password)
+    rr = createReddit(username, password)
+
 
 def getPosts(subreddit, limit=20):
     submissions = rr.subreddit(subreddit).hot(limit=limit)
     data = map((lambda x: models.Post(x)), submissions)
     return list(data)
 
+
 def getUser(user):
     return models.User(rr.redditor(user))
+
 
 submission = getPosts("askreddit", 10)[0]
 origSubmission = list(rr.subreddit("askreddit").hot(limit=2))[1]
 
+
 def testSubmission():
     submission.pprint()
+
+
 # print(str(submission.getComments()))
 
 testSubmission()
