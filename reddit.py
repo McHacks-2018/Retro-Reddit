53723391b6cc7ac5d6a3c7d93b379d014730d06a
JP <jtsaip@gmail.com>
2018-02-03 17:25:46 -0500

    getComment method

--- reddit.py
+++ reddit.py
@@ -3,6 +3,7 @@ import conf
 import logging
 import models
 import pprint
+from praw.models import MoreComments
 
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
@@ -24,11 +25,25 @@ def login(username, password):
                      username=username,
                      password=password)
 
+def getComment():
+    submissions = rr.subreddit("askreddit").hot(limit = 1)
+    s = list(submissions)[0]
+    s.comment_sort = 'new'
+    s.comments.replace_more(limit=None)
+    comment_queue = s.comments[:]  # Seed with top-level
+
+    while comment_queue:
+        comment = comment_queue.pop(0)
+        print(comment.body)
+        comment_queue.extend(comment.replies)
+    #pprint.pprint(vars(s.comments))
+
+
+getComment()
 
 def getPosts(subreddit, limit=20):
     submissions = rr.subreddit(subreddit).hot(limit=limit)
-    for s in submissions
-        data = map((lambda x: models.Post(x)), submissions)
+    data = map((lambda x: models.Post(x)), submissions)
     return list(data)
 
 
