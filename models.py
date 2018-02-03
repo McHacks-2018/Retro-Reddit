ff9b1240121bdcf3078c965bba8533cab0dcf618
Allan Wang <me@allanwang.ca>
2018-02-03 14:56:10 -0500

    Add login options

---
+++ models.py
@@ -0,0 +1,1 @@
+class TextPost(object):
--- reddit.py
+++ reddit.py
@@ -8,9 +8,22 @@ logger = logging.getLogger('retroreddit')
 logger.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
-reddit = praw.Reddit(client_id=conf.clientId,
+rr = praw.Reddit(client_id=conf.clientId,
+                 client_secret=conf.clientSecret,
+                 user_agent=conf.userAgent)
+
+
+def login(username, password):
+    logger.debug("Logging in as {}".format(username))
+    rr = praw.Reddit(client_id=conf.clientId,
                      client_secret=conf.clientSecret,
-                     user_agent=conf.userAgent)
-print(reddit.read_only)
+                     user_agent=conf.userAgent,
+                     username=username,
+                     password=password)
+
 for submission in reddit.subreddit('learnpython').hot(limit=10):
     print(submission.title)
+
+
+def getPosts(subreddit, limit=20):
+    return rr.subreddit(subreddit).hot(limit=limit)
