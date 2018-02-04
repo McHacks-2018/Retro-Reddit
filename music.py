b8ca4508808b27d6ad145f42a37c9534fc21955d
Jongwoo Shim <jongwoo.shim@mail.mcgill.ca>
2018-02-03 21:55:52 -0500

    clear added to music.py

--- music.py
+++ music.py
@@ -3,12 +3,12 @@ import subprocess as sb
 def playMusic():
     try:
         print (sb.check_output(['mpd']))
-        print sb.check_output(['mpd'])
     except sb.CalledProcessError as e:
         pass
     p = sb.Popen(['mpc','-q', 'load'], stdout=sb.PIPE, stdin=sb.PIPE, stderr=sb.STDOUT)    
     # Create m3u file and replace with mpc load
     # Come back to point file later
+    sb.check_output(['mpc','clear'])
     grep_stdout = p.communicate(input=b'retro.m3u')[0]
     print(grep_stdout.decode())
     sb.check_output(['mpc','-q', 'shuffle'])
