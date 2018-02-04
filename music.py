d05b0088870e6e4480dd3fe84f7f4cae229d9c9a
Jongwoo Shim <jongwoo.shim@mail.mcgill.ca>
2018-02-03 21:46:58 -0500

    music.py file setup, retro.m3u setup

---
+++ music.py
@@ -0,0 +1,24 @@
+import subprocess as sb
+
+def playMusic():
+    try:
+        print (sb.check_output(['mpd']))
+        print sb.check_output(['mpd'])
+    except sb.CalledProcessError as e:
+        pass
+    p = sb.Popen(['mpc','-q', 'load'], stdout=sb.PIPE, stdin=sb.PIPE, stderr=sb.STDOUT)    
+    # Create m3u file and replace with mpc load
+    # Come back to point file later
+    grep_stdout = p.communicate(input=b'retro.m3u')[0]
+    print(grep_stdout.decode())
+    sb.check_output(['mpc','-q', 'shuffle'])
+    sb.check_output(['mpc','-q', 'play'])
+
+def stopMusic():
+    sb.check_output(['mpc', 'clear'])
+=======
+    print sb.check_output(['mpc','-q', 'shuffle'])
+    print sb.check_output(['mpc','-q', 'play'])
+
+def stopMusic():
+    print sb.check_output(['mpc', 'clear'])
