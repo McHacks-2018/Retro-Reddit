f31a590a2f9b95ee74ee768dcf1d224f140d00c0
Jongwoo Shim <jongwoo.shim@mail.mcgill.ca>
2018-02-03 19:05:10 -0500

    Left Pane

--- main.py
+++ main.py
@@ -9,10 +9,20 @@ with Cursebox() as cb:
     width, height = cb.width, cb.height
 
     def login_screen():
-        cb.clear();
+        cb.clear()
         # todo define login form
     
     def show_subreddits(subreddits, selected):
+        y = 0
+        x = 0
+        for subreddit in subreddits:
+            bg = colors.black if y!= selected else colors.blue
+            title = post.title[:0] + '...' if len(post.title) > width/3 else post.title.ljust(int(width/3))
+            cb.put(0, y, title, fg = colors.white, bg)
+            y+= 1
+
+
+    # def get_subreddits():
 
 
     def show_posts(posts, selected):
@@ -21,10 +31,10 @@ with Cursebox() as cb:
         for post in posts:
             bg = colors.black if y != selected else colors.blue
             title = post.title[:int(width/3 - 3)] + '...' if len(post.title) > width/3 else post.title.ljust(int(width/3))
-            cb.put(x=x, y=y, text=title, fg=colors.white, bg=bg)
+            cb.put(width/3, y, title, colors.white, bg)
             y += 1
 
-    def show_content(content, scroll_index):
+    # def show_content(content, scroll_index):
 
 
     def show_panes():
