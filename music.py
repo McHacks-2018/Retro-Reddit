6f8357bea0a91b08fe1948f2e8a784e16940a839
Jongwoo Shim <jongwoo.shim@mail.mcgill.ca>
2018-02-03 23:33:50 -0500

    Fixed print statements

--- music.py
+++ music.py
@@ -10,15 +10,9 @@ def playMusic():
     # Come back to point file later
     sb.check_output(['mpc','clear'])
     grep_stdout = p.communicate(input=b'retro.m3u')[0]
-    print(grep_stdout.decode())
+    print (grep_stdout.decode())
     sb.check_output(['mpc','-q', 'shuffle'])
     sb.check_output(['mpc','-q', 'play'])
 
 def stopMusic():
-    sb.check_output(['mpc', 'clear'])
-=======
-    print sb.check_output(['mpc','-q', 'shuffle'])
-    print sb.check_output(['mpc','-q', 'play'])
-
-def stopMusic():
-    print sb.check_output(['mpc', 'clear'])
+    print (sb.check_output(['mpc', 'clear']))
